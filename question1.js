9b733b9bf192b79b07e18d736ad5838d8cc2a6f5
hariadabal <hariadabala2403@gmail.com>
2025-03-25 14:10:28 +0530

    first question is added

---
+++ question1.js
@@ -0,0 +1,12 @@
+function codingScoreCheck(marks, cutoffScore) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            let averageScore = marks.reduce((sum, mark) => sum + mark, 0)
+            if(averageScore >= cutoffScore) {
+                resolve(averageScore)
+            } else {
+                reject("Sorry you haven't cleared the coding round.")
+            }
+        }, 20);
+    });
+}
