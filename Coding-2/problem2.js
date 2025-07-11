92236232001a8721648e74e7fffb7a273f00afbe
hariadabal <hariadabala2403@gmail.com>
2025-07-11 17:56:16 +0530

    assignment is added

--- Coding-2/problem2.js
+++
@@ -1,3 +0,0 @@
-const userNames = [' Alice ', 'bob', ' Charlie', 'alice', 'BOB ']
-let finalAns = [ ... new Set(userNames.map(username => username.trim().toLowerCase()))].sort()
-console.log(finalAns)
